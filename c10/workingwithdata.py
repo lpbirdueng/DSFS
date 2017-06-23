cb33b9ca8bf873bee561b210255779513dc292e7
lpbirdueng <ro.hou@hotmail.com>
2017-06-23 22:59:30 +0800

    update

--- c10/workingwithdata.py
+++ c10/workingwithdata.py
@@ -66,6 +66,20 @@ def correlation_matrix(data):
     return make_matrix(num_columns, num_columns, matrix_entry)
 
 
+def try_parse_field(field_name, value, parser_dict):
+    """try to parse value using the appropriate function from parser_dict"""
+    parser = parser_dict.get(field_name)        # None if no such entry
+    if parser is not None:
+        return try_or_none(parser)(value)
+    else:
+        return value
+
+
+def parse_dict(input_dict, parser_dict):
+    return { field_name : try_parse_field(field_name, value, parser_dict)
+             for field_name, value in input_dict.iteritems()}
+
+
 if __name__ == '__main__':
     data = []
     with open("comma_delimited_stock_prices.csv", newline='') as f:
